37282f3fae012a69e856d60eee7114ca41fc3971
antoine_dh <antoine.dhollande@talan.com>
2024-09-23 18:20:18 +0200

    Update REDME

--- src/python/demo/bo.py
+++ src/python/demo/bo.py
@@ -6,3 +6,6 @@ class PetstoreOperation(BaseRESTOperation):
 
     def init_client(self, adapter):
         return PetstoreClient(adapter)
+        # Alternatively use python requests as the HTTP backend
+        # but lose the capabilities of the adapter
+        # return PetstoreClient(adapter, client=RequestsClient())
